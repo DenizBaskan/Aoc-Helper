3454b03744cc44558e5bb6968b2e9e80575af8de
DenizBaskan <119546222+DenizBaskan@users.noreply.github.com>
2022-12-03 10:57:22 +0000

    Update misc.go

--- misc.go
+++ misc.go
@@ -6,9 +6,7 @@ func getLines(rawInput string) []string {
 	var lines []string 
 
 	for _, v := range strings.Split(rawInput, "\n") {
-		if v != "" {
-			lines = append(lines, v)
-		}
+		lines = append(lines, v)
 	}
 	
 	return lines
